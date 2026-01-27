585146d58fe32acb3fca9425df44eed0f4a12673
souvik Banerjee <souvikbanerjee@souviks-MacBook-Air.local>
2026-01-27 08:00:23 +0530

    Login and Logout functionality with username and password

--- src/components/web/Navbar.tsx
+++ src/components/web/Navbar.tsx
@@ -1,8 +1,32 @@
 import { Link } from "@tanstack/react-router";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./theme-toggle";
+import { User, onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth } from "@/lib/firebase";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export function Navbar() {
+    const [user, setUser] = useState<User | null>(null)
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+
+    const handleSignout = async () => {
+        await signOut(auth).then(() => {
+            setUser(null);
+            toast("Logged out successfully");
+        }).catch((error) => {
+            console.log(error);
+            toast(error.message)
+        });
+    }
 
     return (
         <nav className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
@@ -13,8 +37,18 @@ export function Navbar() {
                 </div>
                 <div className="flex items-center gap-3">
                     <ThemeToggle />
-                    <Link to="/login" className={buttonVariants({ 'variant': 'secondary' })}>Login</Link>
-                    <Link to="/signup" className={buttonVariants()}>Get Started</Link>
+                    {loading ? null : user ? (
+                        <>
+                            <Button onClick={handleSignout}>Logout</Button>
+                            <Link to="/" className={buttonVariants({ 'variant': 'secondary' })}>Dashboard</Link>
+                        </>
+
+                    ) : (
+                        <>
+                            <Link to="/login" className={buttonVariants({ 'variant': 'secondary' })}>Login</Link>
+                            <Link to="/signup" className={buttonVariants()}>Get Started</Link>
+                        </>
+                    )}
                 </div>
 
             </div>
