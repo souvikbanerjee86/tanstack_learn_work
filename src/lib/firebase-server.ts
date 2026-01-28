851732c375448dbc7af322f17206684bb6e47849
souvik Banerjee <souvikbanerjee@souviks-MacBook-Air.local>
2026-01-28 14:16:13 +0530

    modified

--- src/lib/firebase-server.ts
+++ src/lib/firebase-server.ts
@@ -2,17 +2,32 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 
+// if (!getApps().length) {
+//     console.log(process.env.APP_FIREBASE_PROJECT_ID)
+//     console.log(process.env.APP_FIREBASE_CLIENT_EMAIL)
+//     console.log(process.env.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'))
+//     initializeApp({
+//         credential: cert({
+//             projectId: process.env.APP_FIREBASE_PROJECT_ID,
+//             clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL,
+//             // Handle newline characters in private key
+//             privateKey: process.env.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//         }),
+//     });
+// }
+
+
 if (!getApps().length) {
-    console.log(process.env.APP_FIREBASE_PROJECT_ID)
-    console.log(process.env.APP_FIREBASE_CLIENT_EMAIL)
-    console.log(process.env.APP_FIREBASE_PRIVATE_KEY)
+    const serviceAccountString = process.env.APP_FIREBASE_SERVICE_ACCOUNT_JSON;
+
+    if (!serviceAccountString) {
+        throw new Error(
+            "The FIREBASE_SERVICE_ACCOUNT_JSON environment variable is not set."
+        );
+    }
+    const serviceAccount = JSON.parse(serviceAccountString);
     initializeApp({
-        credential: cert({
-            projectId: process.env.APP_FIREBASE_PROJECT_ID,
-            clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL,
-            // Handle newline characters in private key
-            privateKey: process.env.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
+        credential: cert(serviceAccount),
     });
 }
 
