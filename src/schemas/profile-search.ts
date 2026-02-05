5db36252df512e7c5e9c01ac0230892d4a0bf197
souvik Banerjee <souvikbanerjee@souviks-MacBook-Air.local>
2026-02-05 16:38:08 +0530

    Discover page

---
+++ src/schemas/profile-search.ts
@@ -0,0 +1,8 @@
+import z from 'zod'
+
+export const profileSearchSchema = z.object({
+    jobDescription: z.string().min(50, "Job Description must be at least 50 characters long"),
+    preferedDomain: z.string().min(1, "Prefered Domain is required"),
+    skills: z.string().min(1, "Skills are required"),
+    experience: z.number()
+})
