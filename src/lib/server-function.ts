95ca7e15c6a775666130d01a94748fe58914f0a0
souvik Banerjee <souvikbanerjee@souviks-MacBook-Air.local>
2026-02-05 22:50:38 +0530

    added db back

--- src/lib/server-function.ts
+++ src/lib/server-function.ts
@@ -2,7 +2,7 @@ import { createServerFn } from '@tanstack/react-start';
 import { GoogleAuth } from 'google-auth-library';
 import { API_PATH } from './api-path';
 import { BucketListResponse, ProfileSearchResponse, RagProcessRecord } from './types';
-import { db } from './db.server'
+// import { db } from './db.server'
 const auth = new GoogleAuth();
 
 export const fetchBucketListInfo = createServerFn({ method: 'GET' }).handler(async (): Promise<BucketListResponse> => {
@@ -48,30 +48,30 @@ export const getSearchProfileDetails = createServerFn({ method: 'GET' })
     })
 
 
-export const getProcessedIndex = createServerFn({ method: 'GET' }).handler(async (): Promise<RagProcessRecord[]> => {
-    try {
-        const data = await db.collection('rag_file_updates')
-            .orderBy('processed_at', 'desc')
-            .get()
+// export const getProcessedIndex = createServerFn({ method: 'GET' }).handler(async (): Promise<RagProcessRecord[]> => {
+//     try {
+//         const data = await db.collection('rag_file_updates')
+//             .orderBy('processed_at', 'desc')
+//             .get()
 
-        const uniqueMap = new Map()
+//         const uniqueMap = new Map()
 
-        data.docs.forEach(doc => {
-            const data = doc.data()
-            const key = data.date
-            if (!uniqueMap.has(key)) {
-                uniqueMap.set(key, {
-                    id: doc.id,
-                    ...data,
-                    processed_at: data.processed_at?.toDate().toISOString()
-                })
-            }
-        })
-        return Array.from(uniqueMap.values())
+//         data.docs.forEach(doc => {
+//             const data = doc.data()
+//             const key = data.date
+//             if (!uniqueMap.has(key)) {
+//                 uniqueMap.set(key, {
+//                     id: doc.id,
+//                     ...data,
+//                     processed_at: data.processed_at?.toDate().toISOString()
+//                 })
+//             }
+//         })
+//         return Array.from(uniqueMap.values())
 
 
-    } catch (error) {
-        console.error('Error fetching Firestore collection:', error)
-        throw new Error('Internal Server Error')
-    }
-})
+//     } catch (error) {
+//         console.error('Error fetching Firestore collection:', error)
+//         throw new Error('Internal Server Error')
+//     }
+// })
