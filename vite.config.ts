ebe7672832acdef7172049d081e5c9744845dc15
Souvik Banerjee <souvik.mlindia@gmail.com>
2026-02-13 12:11:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,22 @@ const config = defineConfig({
   plugins: [
     devtools(),
     nitro({
-      noExternals: ['firebase-admin']
+      noExternals: ['firebase-admin'],
+      rollupConfig: {
+        output: {
+          format: 'es',
+          banner: `
+            import { createRequire as __createRequire } from 'module';
+            import { fileURLToPath as __fileURLToPath } from 'url';
+            import { dirname as __pathDirname } from 'path';
+            
+            const require = __createRequire(import.meta.url);
+            const __filename = __fileURLToPath(import.meta.url);
+            const __dirname = __pathDirname(__filename);
+          `
+        }
+      }
+    }),
     }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
