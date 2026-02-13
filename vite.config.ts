c181ab1153fae415b5e5ce10fd929506bec643ec
Souvik Banerjee <souvik.mlindia@gmail.com>
2026-02-13 10:27:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,9 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    // nitro({
-    //   noExternals: ['firebase-admin']
-    // }),
+    nitro({
+      noExternals: ['firebase-admin']
+    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
