dfe3c5e0285b99b019ec34ef3f81e024221feebe
Souvik Banerjee <souvik.mlindia@gmail.com>
2026-02-13 11:23:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,8 @@ const config = defineConfig({
   plugins: [
     devtools(),
     nitro({
-      noExternals: ['firebase-admin','@google-cloud/dialogflow-cx', 'google-gax']
+      preset: 'node-server',
+      noExternals: ['firebase-admin']
     }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
@@ -42,7 +43,17 @@ const config = defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['@google-cloud/dialogflow-cx', 'google-gax'],
+      output: {
+        format: 'es',
+        banner: `
+          import { createRequire } from 'module';
+          const require = createRequire(import.meta.url);
+          import { fileURLToPath } from 'url';
+          import { dirname } from 'path';
+          const __filename = fileURLToPath(import.meta.url);
+          const __dirname = dirname(__filename);
+        `,
+      },
     },
   },
 })
