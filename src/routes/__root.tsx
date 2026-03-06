f2e7e28e6f6522a03569103c288d2d2f00f3e368
souvik Banerjee <souvikbanerjee@souviks-MacBook-Air.local>
2026-03-06 12:47:13 +0530

    new code changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, Scripts, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { QueryClientProvider, type QueryClient } from '@tanstack/react-query'
 
 
 import appCss from '../styles.css?url'
 import { ThemeProvider } from '@/lib/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
   head: () => ({
     meta: [
       {
@@ -29,8 +30,18 @@ export const Route = createRootRoute({
     ],
   }),
 
+  component: RootComponent,
   shellComponent: RootDocument,
 })
+
+function RootComponent() {
+  const { queryClient } = Route.useRouteContext()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  )
+}
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
--- src/routes/dashboard/jobs/index.tsx
+++ src/routes/dashboard/jobs/index.tsx
@@ -1,33 +1,41 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
+
+
+export const jobsQueryOptions = queryOptions({
+  queryKey: ['jobs'],
+  queryFn: () => getJobDetails({ data: { limit: null, status: null, last_doc_id: null } }),
+})
 
 export const Route = createFileRoute('/dashboard/jobs/')({
+
+  loader: ({ context }) =>
+    context.queryClient.ensureQueryData(jobsQueryOptions),
   component: RouteComponent,
-  loader: async () => {
-    const data: PaginatedJobResponse = await getJobDetails({ data: { limit: null, status: null, last_doc_id: null } })
-    return { data }
-  }
 })
 
 
 import { columns } from "@/components/web/columns"
 import { DataTable } from "@/components/web/data-table"
 import { getJobDetails } from '@/lib/server-function'
-import { PaginatedJobResponse } from '@/lib/types'
 import { Button } from '@/components/ui/button'
+import { JobTableSkeleton } from '@/components/web/Job-table-skeleton'
+import { Suspense } from 'react'
 
 
 function RouteComponent() {
-  const { data } = Route.useLoaderData()
+  const { data } = useSuspenseQuery(jobsQueryOptions)
+
   return (
-    <div className="container max-w-full">
+    <Suspense fallback={<JobTableSkeleton />}> <div className="container max-w-full">
       <div className="flex justify-between items-center mb-4">
         <p className="text-2xl font-semibold">Job Details</p>
         <Link to='/dashboard/jobs/add'>
           <Button>Add Job</Button>
         </Link>
       </div>
-
       <DataTable columns={columns} data={data.data} />
-    </div>
+    </div> </Suspense>
+
   )
 }
