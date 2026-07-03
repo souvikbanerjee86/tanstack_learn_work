eaa1b71d4b17ddacbf088726ed907fbca7ba281b
souvik Banerjee <souvikbanerjee@souviks-MacBook-Air.local>
2026-07-03 15:34:24 +0530

    code changes for url

--- src/lib/api-path.ts
+++ src/lib/api-path.ts
@@ -1,123 +1,123 @@
 export const API_PATH = {
     BUCKET_LIST_API: {
-        GET_BASE_URL: "https://bucket-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://bucket-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/list-objects?prefix=uploads"
     },
     RAG_SEARCH_API: {
-        GET_BASE_URL: "https://rag-search-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://rag-search-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/rag_search"
     },
     PROCESSED_FILES_ID: {
-        GET_BASE_URL: "https://indexed-files-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://indexed-files-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/indexed-list-api"
     },
     TRIGGER_INDEX: {
-        GET_BASE_URL: "https://trigger-index-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://trigger-index-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/trigger-indexing",
-        CORPUS_ID: "2305843009213693952"
+        CORPUS_ID: "1939767209815441408"
     },
     JOB_DETAILS: {
-        GET_BASE_URL: "https://jobs-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://jobs-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/jobs-list-api"
     },
     JOB_INTERVIEW_CANDIDATES: {
-        GET_BASE_URL: "https://job-interview-candidates-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://job-interview-candidates-1081651239029.us-central1.run.app",
         PATH_URL: "/api/process-candidates"
     },
     JOB_INTERVIEW_CANDIDATE_EMAIL_LIST: {
-        GET_BASE_URL: "https://job-interview-candidate-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://job-interview-candidate-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/job-interview-candidates-list"
     },
     DOWNLOAD_FILE_URL: {
-        GET_BASE_URL: "https://file-url-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://file-url-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/file-url"
     },
     SPEECH_TO_TEXT: {
-        GET_BASE_URL: "https://speech-to-text-api-git-820672860724.europe-west1.run.app",
+        GET_BASE_URL: "https://speech-to-text-api-git-1081651239029.us-central1.run.app",
         PATH_URL: "/api/transcribe-v2"
     },
     CANDIDATE_INTERVIEW_ANSWER_LIST: {
-        GET_BASE_URL: "https://interview-answer-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://interview-answer-list-api-git-1081651239029.us-central1.run.app",
         PATH_URL: "/api/answers"
     },
     CANDIDATE_INTERVIEW_VOICE_UTCOME_LIST: {
-        GET_BASE_URL: "https://interview-voice-outcome-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://interview-voice-outcome-list-api-git-1081651239029.us-central1.run.app",
         PATH_URL: "/api/voice-answers"
     },
     ADD_JOB: {
-        GET_BASE_URL: "https://add-job-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://add-job-api-1081651239029.us-central1.run.app",
         PATH_URL: "/jobs"
     },
     FACE_DETECTION: {
-        GET_BASE_URL: "https://face-detection-api-git-820672860724.europe-west1.run.app",
+        GET_BASE_URL: "https://face-detection-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/match"
     },
     USER_ROLE: {
-        GET_BASE_URL: "https://user-role-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://user-role-api-1081651239029.us-central1.run.app",
         PATH_URL: "/user/role/"
     },
     CANDIDATE_LIST: {
-        GET_BASE_URL: "https://candidate-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://candidate-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/candidates"
     },
     QUESTION_LIST: {
-        GET_BASE_URL: "https://job-interview-questions-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://job-interview-questions-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/jobs/"
     },
     QUESTION_ADD: {
-        GET_BASE_URL: "https://job-interview-question-add-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://job-interview-question-add-api-1081651239029.europe-west1.run.app",
         PATH_URL: "/api/questions/"
     },
     QUESTION_DELETE: {
-        GET_BASE_URL: "https://job-interview-question-delete-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://job-interview-question-delete-api-1081651239029.europe-west1.run.app",
         PATH_URL: "/api/questions/"
     },
     ADD_CANDIDATE: {
-        GET_BASE_URL: "https://rag-resume-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://rag-resume-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/upload"
     },
     QUESTION_ADD_AI: {
-        GET_BASE_URL: "https://add-ai-questions-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://add-ai-questions-1081651239029.us-central1.run.app",
         PATH_URL: "/api/generate-questions"
     },
     DASHBOARD_SUMMARY: {
-        GET_BASE_URL: "https://dashboard-graph-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://dashboard-graph-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/dashboard/summary"
     },
     INTERVIEW_EVALUTE: {
-        GET_BASE_URL: "https://interview-evaluation-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://interview-evaluation-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/evaluate"
     },
     EMAIL_SYNC: {
-        GET_BASE_URL: "https://email-applied-job-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://email-applied-job-1081651239029.us-central1.run.app",
         PATH_URL: "/api/email-applied-job"
     },
     EDIT_JOB: {
-        GET_BASE_URL: "https://edit-job-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://edit-job-api-1081651239029.us-central1.run.app",
         PATH_URL: "/jobs"
     },
     MOVEMENT_OUTCOME: {
-        GET_BASE_URL: "https://interview-movement-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://interview-movement-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/movement-detection"
     },
     DOWNLOAD_VOICE_FILE_URL: {
-        GET_BASE_URL: "https://voice-file-url-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://voice-file-url-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/voice-file-url"
     },
     ADMIN_USER_LIST: {
-        GET_BASE_URL: "https://admin-users-list-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://admin-users-list-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/admin-users-list"
     },
     ADMIN_USER_ACTIVITY: {
-        GET_BASE_URL: "https://admin-users-activity-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://admin-users-activity-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/user-roles/"
     },
     INTERVIEW_CONFIG_SET: {
-        GET_BASE_URL: "https://house-keeping-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://house-keeping-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/config"
     },
     INTERVIEW_CONFIG_GET: {
-        GET_BASE_URL: "https://house-keeping-api-403179946278.europe-west1.run.app",
+        GET_BASE_URL: "https://house-keeping-api-1081651239029.us-central1.run.app",
         PATH_URL: "/api/config"
     }
 }
